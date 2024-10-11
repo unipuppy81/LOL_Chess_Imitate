1904ee3e277851d7742fc3fc13c35eb09c3493aa
kimdaeyeon <extract08@naver.com>
2024-10-11 13:43:13 +0900

    Revert "대연"
    This reverts commit 930ca8bb649b58333b929584038bb73ea2b83c95, reversing
    changes made to 553eec3c14c37e65eaa5a2d931ef11fe73803459.

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -2,11 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager
+public class GameManager : MonoBehaviour
 {
-    [SerializeField] private List<GameObject> myChampionList;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
-    public List<GameObject> MyChampionList => myChampionList;
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
 
 
